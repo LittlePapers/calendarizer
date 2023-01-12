2ad5c3b3a4ce5529d8d4bc0f133bf9fe0cd62615
David Aparicio <davidaapariciobak@gmail.com>
2023-01-11 22:41:23 -0300

    wip: playing with tailwind css to make a card

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import upload from './assets/file_upload.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex justify-center items-center w-2/3 h-screen mx-auto">
+      <div className="bg-white p-4 flex flex-col rounded-lg shadow-xl flex-1">
+        <div className="bg-sky-400 flex rounded-lg flex-col items-center justify-center p-12">
+          <img src={upload} className="h-20 w-20 mx-auto my-4" />
+          <h2 className="text-black mb-4 text-xl font-sans w-40">
+            Drag and drop file here to upload
+          </h2>
+        </div>
+
+        <div className="bg-green-600 mt-2 py-7 flex flex-center justify-center rounded-lg">
+          <img src={reactLogo} />
+          <h4 className="text-black text-xl font-sans ml-4">
+            Select from computer
+          </h4>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
