7e4b737a167f9cdf43ffcb5bac6564ccce7c4403
David Aparicio <davidaapariciobak@gmail.com>
2023-01-19 00:19:34 -0300

    added color picker

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import { DropImageZone } from "./DropImageZone";
 import { UploadImageButton } from "./UploadImageButton";
 import { CanvasEditor } from "./CanvasEditor";
 import { RadioGroup } from "./RadioGroup";
+import { ColorPicker } from "./ColorPicker";
 
 export {
   DropImageZone,
   UploadImageButton,
   CanvasEditor,
   RadioGroup,
+  ColorPicker,
 }
--- src/screens/Editor/Editor.tsx
+++ src/screens/Editor/Editor.tsx
@@ -1,14 +1,13 @@
 import { fabric } from 'fabric';
 import { useEffect, useState, useRef } from 'react';
-import { CanvasEditor, RadioGroup } from '../../components';
+import { CanvasEditor, ColorPicker, RadioGroup } from '../../components';
 import { getNewCalendar, getMonthsGroup } from '../../common/utils';
 import { LAYOUT_OPTIONS } from '../../common/types';
-import { ColorChangeHandler, ColorResult, SketchPicker } from 'react-color';
+import { ColorResult } from 'react-color';
 
 const Editor = () => {
   const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
   const [calendar, setCalendar] = useState<fabric.Group | null>(null);
-  const [displayColorPicker, setDisplayColorPicker] = useState<boolean>(false);
   const [file, setFile] = useState<string>('');
   const buttonRef = useRef<HTMLAnchorElement>(null);
 
@@ -76,22 +75,13 @@ const Editor = () => {
       currentColor: color?.hex,
     });
   };
-
-  const handleClick = () => {
-    setDisplayColorPicker(!displayColorPicker);
-  };
-
-  const handleClose = () => {
-    setDisplayColorPicker(false);
-  };
-
   return (
     <div className="h-screen">
       {file && (
         <div className="relative bg-slate-800 h-full flex flex-col items-center justify-center p-4">
-          <div className="flex flex-col absolute top-5 right-2 z-10">
+          <div className="flex flex-col absolute top-5 right-3 z-10">
             <a
-              className="mt-2 text-black bg-slate-400 rounded-sm px-2 py-1 z-10"
+              className="mt-2 text-black bg-slate-400 rounded-sm px-2 py-1 z-10 text-center"
               ref={buttonRef}
               onClick={exportImage}
             >
@@ -111,29 +101,11 @@ const Editor = () => {
             </div>
 
             <div className="mt-2 text-white">
-              <div
-                className={
-                  'p-2 bg-white rounded-sm shadow inline-block cursor-pointer'
-                }
-                onClick={handleClick}
-              >
-                <div
-                  className={'w-10 h-5 rounded-sm '}
-                  style={{ backgroundColor: currentOptions?.currentColor }}
-                />
-              </div>
-              {displayColorPicker ? (
-                <div className={'absolute z-10 right-0'}>
-                  <div
-                    className={'fixed top-0 right-0 bottom-0 left-0'}
-                    onClick={handleClose}
-                  />
-                  <SketchPicker
-                    color={currentOptions?.currentColor}
-                    onChange={handleColorChange}
-                  />
-                </div>
-              ) : null}
+              <ColorPicker
+                currentColor={currentOptions?.currentColor}
+                handleColorChange={handleColorChange}
+                titleText={'background '}
+              />
             </div>
           </div>
           <CanvasEditor className="w-full h-full" onReady={onReady} />
